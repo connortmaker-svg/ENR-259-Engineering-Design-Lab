f2260306f07b5de1d176ec86efe082d4fd4a4628
nlwizard123 <nlsears@pm.me>
2026-03-30 12:51:18 -0400

    updated main code

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,4 +1,4 @@
-add // ---------------------------------------------
+// ---------------------------------------------
 // main_code.ino
 // ---------------------------------------------
 // This is the main code for the project!
@@ -19,9 +19,9 @@ add // ---------------------------------------------
 #include <Adafruit_SSD1306.h>
 
 // for Pixy2
-// #include <Pixy2.h>
+// #include <Pixy2SPI_SS.h>
 
-// for servo
+// for servo and vacuum
 // #include <Servo.h>
 
 // ---------------------------------------------
@@ -29,9 +29,9 @@ add // ---------------------------------------------
 // ---------------------------------------------
 
 // OLED display height and width in pixels
-#define SCREEN_WIDTH 128 
+#define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
-#define OLED_RESET -1 // reset pin #
+#define OLED_RESET -1  // reset pin #
 #define SCREEN_ADDRESS 0x3C
 
 // Color signature integer values for Pixy
@@ -39,12 +39,19 @@ add // ---------------------------------------------
 #define WHITE 2
 #define BLUE 3
 
+
+// for debug display and vacuum
+#define BAUD_RATE_0 9600
+
+// for the servo wheels, pixy, and servo sort
+// #define BAUD_RATE_1 115200
+
 // ---------------------------------------------
 // Global Variables
 // ---------------------------------------------
 
 // ---------------------------------------------
-// Define Objects (?)
+// Define Objects
 // ---------------------------------------------
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL)
@@ -56,8 +63,31 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Pixy2 pixy;
 // Servo sorting_servo;
 
+// Vacuum: (assuming we're using Servo library)
+// Servo vacuum;
+//
+// Or using turbine control
+// TurbineControl turbine(9)
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(BAUD_RATE_0);
+  delay(500);  // wait for display
+               // Configure display:
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+  Serial.println("Display ready");
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Display ready");
+  display.display();
+
 
 }
 
@@ -65,3 +95,50 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// ---------------------------------------------
+// Display Functions
+// ---------------------------------------------
+
+// display color detected by pixy (might remove and replace with existing pixy function)
+//void display_color_detected(int color) {
+
+//  display.println("Color detected: ");
+//  display.setCursor(100, 10);
+//  switch (signature) {
+//    case RED:
+//      display.println("R");
+//      break;
+//    case WHITE:
+//      display.println("W");
+//      break;
+//    case BLUE:
+//      display.println("B");
+//      break;
+//    default:
+//      display.println("N");
+//      break;
+//  }
+//
+//  display.display();
+//}
+
+void display_vacuum_speed(float pulse) {
+  float conversion_factor = 0.06;
+  float conversion_intercept = 20;
+  float level;
+  level = floor(pulse * conversion_factor - conversion_intercept);
+
+  display.println("Motor level: ");
+  display.setCursor(85, 0);
+  display.println(level);
+  display.display();
+}
+
+  // display current battery percentage
+  void battery_level(float percentage) {
+    display.println("Batt level: ");
+    display.setCursor(80, 20);
+    display.println(percentage);
+    display.display();
+  }
