5e4404b9e2db3e8194b9fd2538fd4e3daafbdd16
KStrowbridge <kylestrow@gmail.com>
2026-04-01 17:39:40 -0400

    Add files via upload

---
+++ Arduino Test Code/Pixy_2/item_recognition_p2/interrupt_item_recognition_p2.ino
@@ -0,0 +1,71 @@
+//NOTE:
+//Object should be 3.5 cm away from object measures from dimaeter to pixy (not the camera, but to the board itself)
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <TimerOne.h>
+#include <Servo.h>
+#include <Pixy2SPI_SS.h>
+
+
+//Pixy object 
+Pixy2SPI_SS pixy;
+//Servo object
+Servo myservo;
+
+
+volatile uint16_t signature = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("Starting...\n");
+  myservo.attach(9);
+
+  pixy.init();
+  //set lamp to max for best item detection
+  pixy.setLamp(255, 255);
+
+  //Initialize timer for the pixy2 with a period of 1ms
+  Timer1.initialize(1000);
+  //Interrupt that reads the pixy2 camera every millisecond (1kHz)
+  Timer1.attachInterrupt(timerISR);
+  Timer1.start();
+}
+
+//Timer ISR that will read the pixy camera and will change the signature if a ball is detected
+void timerISR(){
+  pixy.ccc.getBlocks();
+  if (pixy.ccc.numBlocks > 0){
+    signature = pixy.ccc.blocks[0].m_signature;
+  }
+}
+
+void loop()
+{ 
+  //Switch statment with a case for each ball color (1-red, 2-white, 3-blue)
+  switch (signature) {
+  //Red ball detected
+  case 1:
+    Serial.println("Red ball detected");
+    pixy.ccc.blocks[0].printInfo();
+    myservo.write(50);
+    break;
+  //White ball detected
+  case 2:
+    Serial.println("White ball detected");
+    pixy.ccc.blocks[0].printInfo();
+    myservo.write(100);
+    break;
+  //Blue ball detected
+  case 3:
+    Serial.println("Blue ball detected");
+    pixy.ccc.blocks[0].printInfo();
+    myservo.write(150);
+    break;
+
+  default:  
+    break;
+  }
+}
+
